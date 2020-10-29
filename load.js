40b75943df6f1f30af6a544dc2afe301b592a4ed
AGD <56859084+AdarshGDaniel@users.noreply.github.com>
2020-10-29 18:24:47 +0530

    sixith

--- load.js
+++ load.js
@@ -1,8 +1,14 @@
 const openner = document.getElementById('opener');
+const body = document.getElementById('body');
+
+body.classList.add('notvis');
+
 function load(){
     openner.classList.add('bg-white');
     setTimeout(() => {
         openner.classList.remove('bg-white');
         openner.innerHTML = " ";
+        body.classList.remove('notvis');
     }, 4000);
 }
+
